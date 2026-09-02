78ee76988bcf7315e858502b1da94aa229c1f77a
ytrqua3 <suensenho@gmail.com>
2026-09-02 18:08:43 +0800

    after successful containerize

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def run_migrations():
     command.upgrade(alembic_cfg, "head")
 
 if __name__ == "__main__":
-    #run_migrations()
-    uvicorn.run("scripts.app:app", host="0.0.0.0", port=8080, reload=True)
+    run_migrations()
+    uvicorn.run("scripts.app:app", host="0.0.0.0", port=8000, reload=True)
